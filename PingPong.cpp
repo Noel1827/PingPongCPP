ffaafc431abd8418d138b380406c84d07d13ecaa
Noel1827 <noel.vargas5@upr.edu>
2021-01-08 19:23:50 -0400

    Complete Pong Game

--- PingPong.cpp
+++ PingPong.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
 using namespace std;
 enum eDir{STOP=0,LEFT=1,UPLEFT=2,DOWNLEFT = 3,RIGHT=4,UPRIGHT=5,DOWNRIGHT=6};
 class cBall{
@@ -51,16 +53,215 @@ class cBall{
             y++;
             x++;
             break;
+        default:
+            break;
         }
+    }
+    friend ostream &operator<<(ostream &o, cBall c){
+        o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
+        return o;
+    }
+};
+
+class cPaddle{
+    private:
+int x,y,originalX,originalY;
+public:
+cPaddle(){
+    x=y=0;
+}
+cPaddle(int posX,int posY): cPaddle(){
+    originalX = posX;
+    originalY = posY;
+    x=posX;
+    y=posY;
+}
+
+inline void Reset(){x=originalX; y=originalY;}
+inline int getX(){return x;}
+inline int getY(){return y;}
+inline void moveUp(){y--;}
+inline void moveDown(){y++;}
+    friend ostream &operator<<(ostream &o, cPaddle c){
+        o << " Paddle [" << c.x << "," << c.y << "]";
+        return o;
     }
 
 
-
-
-
 };
-int main(){
 
+class cGameManager{
+    private:
+    int width,height;
+    int score1,score2;
+    char up1,down1,up2,down2;
+    bool quit;
+    cBall* ball;
+    cPaddle* player1;
+    cPaddle* player2;
+
+    public:
+        cGameManager(int w, int h)
+        {
+            srand(time(NULL));
+            quit=false;
+            up1 = 'w'; up2 = 'i';
+            down1 = 's'; down2 = 'k';
+            score1 = score2 = 0;
+            width = w; height = h;
+            ball = new cBall(w/2,h/2);
+            player1 = new cPaddle(1, h / 2 - 3);
+            player2 = new cPaddle(w - 2, h / 2 - 3);
+        }
+
+        ~cGameManager()
+        {
+            delete ball, player1, player2;
+        }
+
+        void ScoreUp(cPaddle* player){
+            if(player == player1)
+            score1++;
+            else if(player == player2)
+            score2++;
+
+            ball->Reset();
+            player1->Reset();
+            player2->Reset();
+        }
+
+        void Draw(){
+            system("cls");
+            for(int i = 0; i < width+2; i++)
+                cout << "\xB2";
+                cout << endl;
+
+                for(int i = 0; i < height; i++){
+                    for(int j = 0; j < width; j++){
+                        int ballx = ball->getX();
+                        int bally = ball->getY();
+                        int player1x = player1->getX();
+                        int player1Y = player1->getY();
+                        int player2x = player2->getX();
+                        int player2Y = player2->getY();
+
+                        if(j==0 )
+                        cout << "\xB2";
+
+                        if (ballx == j && bally == i)
+                            cout << "O"; // ball
+                        else if (player1x == j && player1Y == i)
+                            cout << "\xB4"; // player1
+                        else if (player2x == j && player2Y == i)
+                            cout << "\xC3"; // Player2
+                            
+                        else
+                            cout << " ";
+
+                        if(j==width-1)
+                        cout << "\xB2";
+
+                    }
+                    cout << endl;
+                }
+            
+            for(int i = 0; i < width+2; i++)
+            cout << "\xB2";
+            cout << endl;
+
+            cout << "Score 1: "<< score1<< endl << "Score 2: " << score2 << endl;
+            
+        }
+
+        void Input()
+        {
+            ball->Move();
+            int ballx = ball->getX();
+            int bally = ball->getY();
+            int player1x = player1->getX();
+            int player1Y = player1->getY();
+            int player2x = player2->getX();
+            int player2Y = player2->getY();
+
+            if(_kbhit()){
+                char current = _getch();
+                if (current == up1)
+                    if (player1Y > 0)
+                        player1->moveUp();
+
+                if (current == up2)
+                    if (player2Y > 0)
+                        player2->moveUp();
+
+                if (current == down1)
+                    if (player1Y + 4 < height)
+                        player1->moveDown();
+
+                if (current == down2)
+                    if (player2Y + 4 < height)
+                        player2->moveDown();
+
+                if (ball->getDirection() == STOP)
+                    ball->randonDirection();
+
+                if (current == 'q')
+                    quit = true;
+            }
+        }
+
+        void logic(){
+            int ballx = ball->getX();
+            int bally = ball->getY();
+            int player1x = player1->getX();
+            int player1Y = player1->getY();
+            int player2x = player2->getX();
+            int player2Y = player2->getY();
+
+            // left paddle
+            for (int i = 0; i < 4; i++)
+            {
+                if (ballx == player1x + 1 && bally == player1Y+i)
+                    ball->changeDirection((eDir)((rand() % 3) + 4));
+            }
+            // right paddle
+            for (int i = 0; i < 4; i++)
+            {
+                if (ballx == player2x - 1 && bally == player2Y + i)
+                    ball->changeDirection((eDir)((rand() % 3) + 1));
+            }
+            // bottom wall
+            if (bally == height - 1)
+                ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+
+            // topwall
+            if (bally == 0)
+                ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+
+            // left wall
+            if (ballx == 0)
+                // ball->changeDirection(ball->getDirection() == UPLEFT ? UPRIGHT : DOWNRIGHT);
+                ScoreUp(player2);
+
+            if (ballx == width - 1)
+                ScoreUp(player1);
+            // ball->changeDirection(ball->getDirection() == UPRIGHT ? UPLEFT : DOWNLEFT);
+        }
+
+        void Run()
+        {
+            while (!quit)
+            {
+                Draw();
+                Input();
+                logic();
+            }
+        }
+};
+int main()
+{
+
+    cGameManager c(40, 20);
+    c.Run();
 
     return 0;
 }
